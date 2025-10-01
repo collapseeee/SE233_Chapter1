c697b1da9fd462aea211686777b233864a6977f0
COLLAPse <4174tt@gmail.com>
2025-10-01 21:11:59 +0700

    Update for CH6, and .gitignore

--- chapter1/src/main/java/module-info.java
+++ chapter1/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module se233.chapter1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires jdk.jdi;
+    requires java.logging;
+    requires org.apache.logging.log4j;
 
 
     opens se233.chapter1 to javafx.fxml;
--- chapter1/src/main/java/se233/chapter1/model/character/BasedCharacter.java
+++ chapter1/src/main/java/se233/chapter1/model/character/BasedCharacter.java
@@ -1,9 +1,12 @@
 package se233.chapter1.model.character;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import se233.chapter1.model.item.Armor;
 import se233.chapter1.model.item.Weapon;
 
 public class BasedCharacter {
+    private static final Logger logger = LogManager.getLogger(BasedCharacter.class);
     protected String name, imgpath;
     protected DamageType type;
     protected Integer fullHp, basedPow, basedDef, basedRes;
@@ -20,6 +23,7 @@ public class BasedCharacter {
     public void equipWeapon(Weapon weapon) {
         this.weapon = weapon;
         this.power = this.basedPow + weapon.getPower();
+        logger.info("Equipped weapon: {}, Power: {}, Type: {}.", weapon.getName(), weapon.getPower(), weapon.getDamageType());
     }
     public void unequipWeapon (Weapon weapon) {
         this.weapon = null;
@@ -29,6 +33,7 @@ public class BasedCharacter {
         this.armor = armor;
         this.defense = this.basedDef + armor.getDefense();
         this.resistance = this.basedRes + armor.getResistance();
+        logger.info("Equipped armor: {}, Defense: {}, Resistance: {}.", armor.getName(), armor.getDefense(), armor.getResistance());
     }
     public void unequipArmor (Armor armor) {
         this.armor = null;
